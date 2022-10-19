45fd62b27247fb191706b5c910758795dd55862b
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-10-19 15:46:11 -0500

    nogo

---
+++ API/Cards/Cards.API/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cards.API.Models;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        protected CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // dbset
+        public DbSet<Card> Cards { get; set; }
+
+    }
+}
